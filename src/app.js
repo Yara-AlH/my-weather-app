0b442d67e66e4dce155335b282071e14402bd28a
Yara-AlH <139350596+Yara-AlH@users.noreply.github.com>
2023-08-18 15:40:33 +0200

    added weather forecast

--- src/app.js
+++ src/app.js
@@ -11,7 +11,6 @@ let days = [
 
 let currentDay = days[now.getDay()];
 let currentHour = now.getHours();
-console.log(currentHour);
 let currentMinute = now.getMinutes();
 
 if (currentHour > 7 && currentHour < 19) {
@@ -46,12 +45,15 @@ function getWeather(event) {
 
   let apiKey = "0c6283dt87dcb24afbce90bd2bac3o16";
   let apiURL = `https://api.shecodes.io/weather/v1/current?query=${cityValue}&key=${apiKey}&units=metric`;
-  console.log(apiURL);
-  axios.get(apiURL).then(showWeatherForecast);
+  axios.get(apiURL).then(showWeather);
+
+  let forecastApiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${cityValue}&key=${apiKey}`;
+  axios.get(forecastApiUrl).then(showWeatherForecast);
 }
 
-function showWeatherForecast(response) {
+function showWeather(response) {
   console.log(response.data);
+
   let temp = response.data.temperature.current;
   let icon = response.data.condition.icon;
 
@@ -69,6 +71,59 @@ function showWeatherForecast(response) {
       "src",
       `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${icon}.png`
     );
+
+  getForecast();
+}
+
+function getForecast(response) {
+  let apiKey = "0c6283dt87dcb24afbce90bd2bac3o16";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(showWeatherForecast);
+}
+
+function formatday(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let day = date.getDay();
+
+  return days[day];
+}
+
+function showWeatherForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector(".weather-forecast");
+
+  let forecastHTML = `<div class="row">`;
+  forecast.forEach(function (forecastday, index) {
+    if (index > 0) {
+      forecastHTML =
+        forecastHTML +
+        ` <div class="col">
+          <div class="weather-forecast-date">${formatday(
+            forecastday.time
+          )}</div>
+         <img
+         src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${
+           forecastday.condition.icon
+         }.png"
+         alt=""
+         width="42"
+         />
+         <div class="weather-forecast-temperature">
+           <span class="weather-forecast-temperature-max"> ${Math.round(
+             forecastday.temperature.maximum
+           )}°</span>
+           <span class="weather-forecast-temperature-min"> ${Math.round(
+             forecastday.temperature.minimum
+           )}°</span>
+         </div>
+         </div>`;
+    }
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function handlePosition(position) {
@@ -78,10 +133,13 @@ function handlePosition(position) {
   let apiKey = "0c6283dt87dcb24afbce90bd2bac3o16";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
 
-  axios.get(apiUrl).then(showCoordsForecast);
+  axios.get(apiUrl).then(showCoords);
+
+  let forecastApiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}`;
+  axios.get(forecastApiUrl).then(showWeatherForecast);
 }
 
-function showCoordsForecast(response) {
+function showCoords(response) {
   let temp = response.data.temperature.current;
   let icon = response.data.condition.icon;
 
@@ -126,3 +184,42 @@ function convertToCelsius(event) {
   document.querySelector(".tempNumber").innerHTML =
     Math.round(celsiusTemperature);
 }
+
+// function showWeatherForecast(response) {
+//   let forecastHTML = `<div class="row">`;
+//   response.data.daily.forEach(function (dayData, index) {
+//     if (index < 6) {
+//       // Assuming you want only the next 6 days excluding today
+//       forecastHTML += `<div class="col">
+//                   <div class="weather-forecast-date">${dayData.dt}</div>
+//                   <div class="weather-forecast-temp dayZero">${dayData.temp.day}</div>
+//                   <img src="https://openweathermap.org/img/wn/${dayData.weather[0].icon}@2x.png" alt="${dayData.weather[0].description}" width="42" />
+//                   <div class="weather-forecast-temperature">
+//                       <span class="weather-forecast-temperature-max">${dayData.temp.max}°</span>
+//                       <span class="weather-forecast-temperature-min">${dayData.temp.min}°</span>
+//                   </div>
+//               </div>`;
+//     }
+//   });
+//   forecastHTML = forecastHTML + `</div>`;
+// You will also likely want to inject the generated HTML into your page here, for example:
+// document.querySelector("#forecast").innerHTML = forecastHTML;
+// }
+// let tempOne = response.data.daily[1].temperature.day;
+// let tempMaxOne = response.data.daily[1].temperature.maximum;
+// let tempMinOne = response.data.daily[1].temperature.minimum;
+// let tempTwo = response.data.daily[2].temperature.day;
+// let tempMaxTwo = response.data.daily[2].temperature.maximum;
+// let tempMinTwp = response.data.daily[2].temperature.minimum;
+// let tempThree = response.data.daily[3].temperature.day;
+// let tempMaxThree = response.data.daily[3].temperature.maximum;
+// let tempMinThree = response.data.daily[3].temperature.minimum;
+// let tempFour = response.data.daily[4].temperature.day;
+// let tempMaxFour = response.data.daily[4].temperature.maximum;
+// let tempMinFour = response.data.daily[4].temperature.minimum;
+// let tempFive = response.data.daily[5].temperature.day;
+// let tempMaxFive = response.data.daily[5].temperature.maximum;
+// let tempMinFive = response.data.daily[5].temperature.minimum;
+// let tempSix = response.data.daily[6].temperature.day;
+// let tempMaxSix = response.data.daily[6].temperature.maximum;
+// let tempMinSix = response.data.daily[6].temperature.minimum;
